a238bfc8506899e7a3f92b0bc612093d9233d887
Juan Carlos Campos <rapidclimate@gmail.com>
2018-10-15 11:04:17 -0500

    Initial commit

---
+++ src/main/java/org/unitec/seguridad/WebSecurityConfig.java
@@ -0,0 +1,76 @@
+package org.unitec.seguridad;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        UserDetails rob = userBuilder.username("rob").password("rob").roles("USER").build();
+        UserDetails admin = userBuilder.username("admin").password("admin").roles("USER","ADMIN").build();
+
+       // manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        manager.createUser(rob);
+        manager.createUser(admin);
+        return manager;
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/borrego/**")
+                .antMatchers("/publics/**");
+    }
+
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/signup", "/a   bout").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and().httpBasic().and().csrf().disable();
+
+             //   .formLogin();
+        http.cors(); //Este es para que acepte los origenes cruzados en la autenticación
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+
+
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders este es importantisimo sion el se lanza el eeror 403 de origenes no permitidos
+        //
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
